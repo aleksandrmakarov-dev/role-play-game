2cbcb88497860311707e073bda216c8839abca1f
Alex <ac5295@student.jamk.fi>
2024-03-13 20:19:10 +0200

    start

---
+++ RolePlayGame.App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayGame.App
+{
+
+    public class Program
+    {
+
+        static void Main()
+        {
+            Point p = new Point(0, 0);
+
+            const int w = 24;
+            const int h = 24;
+            char[,] buffer = new char[w, h];
+
+            Console.SetWindowSize(w,h);
+            Console.SetBufferSize(w,h);
+
+
+            
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    buffer[i, j] = '0';
+                }
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    Console.SetCursorPosition(i,j);
+                    Console.Write(buffer[i,j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RolePlayGame.Graphics/Screen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayGame.Graphics
+{
+    public class Screen
+    {
+        private readonly int _width = 120;
+        private readonly int _height = 30;
+
+
+    }
+}
