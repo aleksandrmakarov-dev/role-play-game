6d96c1ce8a548bed017cffa2d82050acc8e25c89
Alex <ac5295@student.jamk.fi>
2024-03-13 21:31:31 +0200

    graphics

--- RolePlayGame.App/Program.cs
+++ RolePlayGame.App/Program.cs
@@ -3,50 +3,37 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using RolePlayGame.Graphics;
 
 namespace RolePlayGame.App
 {
 
     public class Program
     {
+        private const int _w = 120;
+        private const int _h = 30;
+
+        private static Screen _screen;
+
+        private static Point p = new Point(0,0);
+
+
 
         static void Main()
         {
-            Point p = new Point(0, 0);
+            _screen = new Screen(_w,_h,8,8);
 
-            const int w = 24;
-            const int h = 24;
-            char[,] buffer = new char[w, h];
+            _screen.Update += OnScreenUpdate;
+            _screen.Start();
+        }
 
-            Console.SetWindowSize(w,h);
-            Console.SetBufferSize(w,h);
+        private static void OnScreenUpdate(object? sender, EventArgs e)
+        {
 
-
-            
-
-            for (int i = 0; i < w; i++)
-            {
-                for (int j = 0; j < h; j++)
-                {
-                    buffer[i, j] = '0';
-                }
-            }
-
-            Console.Clear();
-
-            for (int i = 0; i < w; i++)
-            {
-                for (int j = 0; j < h; j++)
-                {
-                    Console.SetCursorPosition(i,j);
-                    Console.Write(buffer[i,j]);
-                }
-            }
-
-            Console.ReadKey();
         }
     }
 }
--- RolePlayGame.Graphics/Screen.cs
+++ RolePlayGame.Graphics/Screen.cs
@@ -1,16 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RolePlayGame.Graphics
+﻿namespace RolePlayGame.Graphics
 {
     public class Screen
     {
-        private readonly int _width = 120;
-        private readonly int _height = 30;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly int _fontWidth;
+        private readonly int _fontHeight;
+
+        private char[,] buffer;
+
+        private Thread _background;
+        private ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
+
+        private const int FrameTime = 500;
+
+        public event EventHandler Update;
+
+        public Screen(int width, int height, int fontWidth, int fontHeight)
+        {
+            _width = width;
+            _height = height;
+            _fontWidth = fontWidth;
+            _fontHeight = fontHeight;
+
+            Initialize();
+        }
 
 
+        public void Stop()
+        {
+            _shutdownEvent.Set();
+        }
+
+        public void Start()
+        {
+            _background.Join();
+        }
+
+        public void Print(int x, int y, string text)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(text);
+        }
+
+        private void Initialize()
+        {
+            buffer = new char[_width, _height];
+
+            Console.SetBufferSize(_width,_height);
+            Console.SetWindowSize(_width,_height);
+            Console.CursorVisible = false;
+
+            _background = new Thread(RunInBackground)
+            {
+                IsBackground = true
+            };
+
+            _background.Start();
+        }
+
+        private void RunInBackground()
+        {
+
+            while (!_shutdownEvent.WaitOne(FrameTime))
+            {
+                Update.Invoke(this,EventArgs.Empty);
+            }
+        }
     }
 }
